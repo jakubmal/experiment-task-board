8d612720fa4b2d5709a97bb0d427ebf6630f1a70
Jakub Malinowski <jakub@malinowski.me>
2017-03-12 19:21:37 +0100

    feat: boards#index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Board.where(name: 'Test board 1').first_or_create!
+Board.where(name: 'Test board 2').first_or_create!
+Board.where(name: 'Test board 3').first_or_create!
+Board.where(name: 'Test board 4').first_or_create!
+Board.where(name: 'Test board 5').first_or_create!
